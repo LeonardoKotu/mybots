b93ec5e27159c837bfe6ce1b9c0ef6cffbfa3d4b
f1sher <100369987+LeonardoKotu@users.noreply.github.com>
2023-05-05 06:01:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-# This example requires the 'message_content' privileged intents
 
-import os
-import discord
-from discord.ext import commands
+"""
+Основная задача:
+Бот медиа должен отследовать сообщении, и заодно создавать ветки.
+UPD: можно реакции добавлять, Погнали!
 
+"""
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+import disnake
+from disnake import guild
+from disnake.ext import commands
 
+intents = disnake.Intents().all() # для бота
+
+# создание бота
+bot = commands.InteractionBot(intents=intents)
+
+#inter = disnake.Interaction
 
 @bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
+async def on_message(message: disnake.Message):
+    #channel = disnake.utils.get(bot.get_all_channels(), name='├💞・милашки')
+    cutie = bot.get_channel(1102004191255343104)
+    media = bot.get_channel(1102004091795804271)
+    photos = bot.get_channel(1102004345844801546)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+    channels = [cutie, media, photos]
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+    if message.channel in channels:
+        if message.attachments:
+           await message.create_thread(name=f"Веточка", auto_archive_duration=10080)
 
 
-bot.run(os.environ["DISCORD_TOKEN"])
+bot.run("MTEwMjk0MjIwNTEyOTI2OTM5OQ.GGM0Rm.tdvqIwlUjiVIrHSypx-L-4NfxhjsqYby_gJttU")
